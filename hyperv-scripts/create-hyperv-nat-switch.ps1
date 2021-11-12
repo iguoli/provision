06961b6acda91bb3ce81f619d7ea6493f2999018
Guo, Li <28505129+iguoli@users.noreply.github.com>
2021-11-13 00:09:49 +0800

    Updated the Vagrantfile of Hyper-V

--- hyperv-scripts/create-hyperv-nat-switch.ps1
+++ hyperv-scripts/create-hyperv-nat-switch.ps1
@@ -7,21 +7,19 @@ If ("NATSwitch" -in (Get-VMSwitch | Select-Object -ExpandProperty Name) -eq $FAL
 
     New-VMSwitch -SwitchName "NATSwitch" -SwitchType Internal
 
-    New-NetIPAddress -IPAddress 192.168.33.10 -PrefixLength 24 -InterfaceAlias "vEthernet (NATSwitch)"
-
-    New-NetNAT -Name "NATNetwork" -InternalIPInterfaceAddressPrefix 192.168.33.0/24
+    New-NetIPAddress -IPAddress 192.168.33.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NATSwitch)"
 }
 else {
     '"NATSwitch" for static IP configuration already exists; skipping'
 }
 
-If ("192.168.33.10" -in (Get-NetIPAddress | Select-Object -ExpandProperty IPAddress) -eq $FALSE) {
-    'Registering new IP address 192.168.33.10 on Windows Hyper-V host...'
+If ("192.168.33.1" -in (Get-NetIPAddress | Select-Object -ExpandProperty IPAddress) -eq $FALSE) {
+    'Registering new IP address 192.168.33.1 on Windows Hyper-V host...'
 
-    New-NetIPAddress -IPAddress 192.168.33.10 -PrefixLength 24 -InterfaceAlias "vEthernet (NATSwitch)"
+    New-NetIPAddress -IPAddress 192.168.33.1 -PrefixLength 24 -InterfaceAlias "vEthernet (NATSwitch)"
 }
 else {
-    '"192.168.33.10" for static IP configuration already registered; skipping'
+    '"192.168.33.1" for static IP configuration already registered; skipping'
 }
 
 If ("192.168.33.0/24" -in (Get-NetNAT | Select-Object -ExpandProperty InternalIPInterfaceAddressPrefix) -eq $FALSE) {
